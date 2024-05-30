badc1aead85a66b5b74d376fc6d554b403b7e6f5
ahmedghazi <atmet.ghazi@gmail.com>
2024-05-30 17:45:34 +0200

    fix

--- web/app/components/CardExhibition.tsx
+++ web/app/components/CardExhibition.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Exhibition } from "../types/schema";
 import { _localizeField } from "../utils/utils";
 import { PortableText } from "next-sanity";
 import portableTextComponents from "../utils/portableTextComponents";
 import Slider from "./ui/slick-slider";
 import Figure from "./ui/Figure";
+import { useInView } from "react-intersection-observer";
 
 type Props = {
   input: Exhibition;
 };
 
 const CardExhibition = ({ input }: Props) => {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
+
+  // useEffect(() => {
+
+  // }, [inView]);
+
   return (
-    <article className='content-article'>
+    <article className='content-exhibition' ref={ref}>
       <div className='grid md:grid-cols-12 gap-md'>
         <aside className='md:col-span-4'>
           <h1 className='mb-md'>{_localizeField(input.title)}</h1>
@@ -24,13 +34,22 @@ const CardExhibition = ({ input }: Props) => {
           </div>
         </aside>
         <div className='media md:col-span-8'>
-          <Slider settingsOverride={{ autoplay: true }}>
-            {input.slider?.map((item, i) => (
-              <div className='slide' key={i}>
-                {item.image && <Figure asset={item.image.asset} />}
-              </div>
-            ))}
-          </Slider>
+          {input.slider?.length === 1 && (
+            <div className='slide'>
+              {input.slider[0].image && (
+                <Figure asset={input.slider[0].image.asset} />
+              )}
+            </div>
+          )}
+          {input.slider && input.slider.length > 1 && inView && (
+            <Slider settingsOverride={{ autoplay: true }}>
+              {input.slider?.map((item, i) => (
+                <div className='slide' key={i}>
+                  {item.image && <Figure asset={item.image.asset} />}
+                </div>
+              ))}
+            </Slider>
+          )}
         </div>
       </div>
     </article>
