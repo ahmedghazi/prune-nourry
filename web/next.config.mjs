efb6a77087eaa418bfdc70007980044a39da8502
ahmedghazi <atmet.ghazi@gmail.com>
2024-11-16 08:34:51 +0100

    fix

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -14,6 +14,15 @@ const nextConfig = {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/fr",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
