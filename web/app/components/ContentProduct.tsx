3ff722b410e7ac03b32b96790df9ce11c441d9eb
ahmedghazi <atmet.ghazi@gmail.com>
2024-11-22 14:03:42 +0100

    fix

--- web/app/components/ContentProduct.tsx
+++ web/app/components/ContentProduct.tsx
@@ -17,30 +17,7 @@ type Props = {
 
 const ContentProduct = ({ input }: Props) => {
   const router = useRouter();
-  // const { cartItems, setCartItems } = useShop();
-  // const [qty, setQty] = useState<number>(1);
 
-  // const _onClick = () => {
-  //   //ajout ou update le produit dans le panier
-  //   if (cartItems.findIndex((el) => el._id === input._id) === -1) {
-  //     setCartItems((prev: ProductExtend[]) => [...prev, input]);
-  //   } else {
-  //     setCartItems((prev: ProductExtend[]) => {
-  //       return prev.map((item: ProductExtend) => {
-  //         return item._id === input._id
-  //           ? { ...item, quantity: qty } //celui sur lequel on on cliqué
-  //           : item; //les autres
-  //       });
-  //     });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   input.quantity = qty;
-  //   if (qty > 1) _onClick();
-  // }, [qty]);
-
-  // console.log(input);
   const isLowStock = input.quantity === 1;
   const isOutOfStock = !input.quantity || input.quantity < 1;
   // console.log(input);
